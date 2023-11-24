3cd48429b77b16dc915079853514d054e6d4b9d5
Felipe Restrepo <ManuelFelipe.Restrepo@carvana.com>
2023-11-24 07:05:16 -0500

    chore: remove boilerplate; feat: bare project

--- apps/trips/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from trips.views import TripView
-
-app_name = "trips"
-urlpatterns = [
-    path("", TripView.as_view({"get": "list"}), name="trip_list"),
-    path("<uuid:trip_id>/", TripView.as_view({"get": "retrieve"}), name="trip_detail"),
-]
