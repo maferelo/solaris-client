5434b5130f1ba3ba1aa278c787c5c93b67d6f081
Felipe Restrepo <ManuelFelipe.Restrepo@carvana.com>
2023-11-25 07:38:48 -0500

    feat(e2e): detox setup with ios test

--- App.js
+++ App.js
@@ -1,22 +1,10 @@
 import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [clicked, setClicked] = useState(false);
-
   return (
     <View style={styles.container}>
-      {!clicked && (
-        <Pressable
-          testID="click-me-button"
-          style={styles.button}
-          onPress={() => setClicked(true)}
-        >
-          <Text style={styles.text}>Click me!</Text>
-        </Pressable>
-      )}
-      {clicked && <Text style={styles.hi}>Clicked!</Text>}
+      <Text testID="welcome">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -28,25 +16,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  hi: {
-    fontSize: 30,
-    color: "#4630EB",
-  },
-  button: {
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-    backgroundColor: "#4630EB",
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: "bold",
-    letterSpacing: 0.25,
-    color: "white",
   },
 });
