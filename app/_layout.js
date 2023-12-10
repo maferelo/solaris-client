6f2dfd3b68014ce2ab3d62ac9f044b7a407317e8
Felipe Restrepo <ManuelFelipe.Restrepo@carvana.com>
2023-12-10 10:34:56 -0500

    fix(android): safeareaview

--- app/_layout.js
+++ app/_layout.js
@@ -1,11 +1,19 @@
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeLayout() {
   return (
-    <>
+    <SafeAreaView style={styles.container}>
       <Slot />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
