65395d23e38a037812b436e5f610b47a70703366
Felipe Restrepo <maferelo@outlook.com>
2024-03-05 18:19:29 -0500

    feat(build): prevent storybook files to be bundled in the app

--- metro.config.js
+++ metro.config.js
@@ -11,8 +11,25 @@ generate({
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Storybook required to enable dynamic story imports
 config.transformer.unstable_allowRequireContext = true;
 
 config.resolver.sourceExts.push('mjs');
+
+// Prevent storybook files from being bundled in the app
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+    const defaultResolveResult = context.resolveRequest(context, moduleName, platform);
+  
+    if (
+      process.env.STORYBOOK_ENABLED !== 'true' &&
+      defaultResolveResult?.filePath?.includes?.('.storybook/')
+    ) {
+      return {
+        type: 'empty',
+      };
+    }
+  
+    return defaultResolveResult;
+  };
 
 module.exports = config;
