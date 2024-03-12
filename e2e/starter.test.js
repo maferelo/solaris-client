2c654e59d7cfc5da4a20c8a4c3d0cdb24d53af05
Felipe Restrepo <maferelo@outlook.com>
2024-03-12 14:53:13 -0500

    fix(e2e): Refactor e2e/starter.test.js to use element indexing

--- e2e/starter.test.js
+++ e2e/starter.test.js
@@ -14,11 +14,11 @@ describe("Example", () => {
   });
 
   it("should have home screen", async () => {
-    await expect(element(by.id("home"))).toBeVisible();
+    await expect(element(by.id("home")).atIndex(0)).toBeVisible();
   });
 
   it("should show navigate to details screen on tap", async () => {
-    await element(by.id("home")).tap();
+    await element(by.id("home")).atIndex(0).tap();
     await expect(element(by.id("details"))).toBeVisible();
   });
 });
