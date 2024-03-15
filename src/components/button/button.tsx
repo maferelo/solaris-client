837df70c6e0e8ee119ee158583822bee6f27937a
Felipe Restrepo <maferelo@outlook.com>
2024-03-15 14:22:33 -0500

    feat(components): Refactor button component to support different variants

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,5 +1,29 @@
-import { Button } from "@rneui/themed";
+import { Button as RNUIButton } from "@rneui/themed";
+import { MouseEventHandler, ReactNode } from "react";
 
-export default () => {
-  return <Button title="Solid" />;
+const variants = {
+  solid: {},
+  outline: {},
+  clear: {},
+};
+
+export type ButtonProps = {
+  children: ReactNode;
+  variant?: keyof typeof variants;
+  isLoading?: boolean;
+  isDisabled?: boolean;
+  onClick?: MouseEventHandler<HTMLButtonElement>;
+  icon?: JSX.Element;
+};
+
+export const Button = ({
+  variant = "solid",
+  children,
+  ...props
+}: ButtonProps) => {
+  return (
+    <RNUIButton {...props} {...variants[variant]} title={variant}>
+      {children}
+    </RNUIButton>
+  );
 };
