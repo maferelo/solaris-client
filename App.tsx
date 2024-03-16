8ef0272b60925f7d7f540e92959dbd83b63709fd
Felipe Restrepo <maferelo@outlook.com>
2024-03-16 15:49:22 -0500

    chore(entrypoint): Refactor navigation in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,32 +1,6 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Constants from "expo-constants";
 
-import { App as Application } from "@/pages/_app";
-import { DetailsScreen } from "@/pages/details";
-import { HomeScreen } from "@/pages/home-screen";
-
-type RootStackParamList = {
-  Home: undefined;
-  Details: undefined;
-};
-
-const Stack = createNativeStackNavigator<RootStackParamList>();
-
-function App() {
-  return (
-    <Application>
-      <NavigationContainer
-        children={
-          <Stack.Navigator initialRouteName="Home">
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Details" component={DetailsScreen} />
-          </Stack.Navigator>
-        }
-      />
-    </Application>
-  );
-}
+import { App } from "@/pages/_app";
 
 let AppEntryPoint = App;
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,33 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ReactNode } from "react";
 
+import { DetailsScreen } from "@/pages/details";
+import { HomeScreen } from "@/pages/home-screen";
 import { AppProvider } from "@/providers/app";
 
 export type AppProps = {
   children: ReactNode;
 };
-export const App = ({ children }: AppProps) => {
-  return <AppProvider>{children}</AppProvider>;
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
 };
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+export function App() {
+  return (
+    <AppProvider>
+      <NavigationContainer
+        children={
+          <Stack.Navigator initialRouteName="Home">
+            <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />
+          </Stack.Navigator>
+        }
+      />
+    </AppProvider>
+  );
+}
