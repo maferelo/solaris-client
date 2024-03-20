769f02bfff0ed8f60c31de20184f8c357a112fc0
Felipe Restrepo <maferelo@outlook.com>
2024-03-20 18:07:35 -0500

    feat(sentry): Refactor App initialization and add Sentry instrumentation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,8 @@
-import * as Sentry from "@sentry/react-native";
 import Constants from "expo-constants";
 
 import { App } from "@/pages/_app";
 
-Sentry.init({
-  dsn: "https://2360ae10da41b9c1950f60c859ce2122@o1208436.ingest.us.sentry.io/4506945207599104",
-  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
-});
-
-let AppEntryPoint = Sentry.wrap(App);
+let AppEntryPoint = App;
 
 if (Constants.expoConfig?.extra?.storybookEnabled === "true") {
   AppEntryPoint = require("./.storybook").default;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as Sentry from "@sentry/react-native";
 import { ReactNode } from "react";
 
 import { DetailsScreen } from "@/pages/details";
@@ -15,12 +19,28 @@ type RootStackParamList = {
   Home: undefined;
 };
 
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+
+Sentry.init({
+  dsn: "https://2360ae10da41b9c1950f60c859ce2122@o1208436.ingest.us.sentry.io/4506945207599104",
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  integrations: [new Sentry.ReactNativeTracing({ routingInstrumentation })],
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  tracesSampleRate: 1.0,
+});
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-export function App() {
+function ProvidedApp() {
+  const navigation = useNavigationContainerRef();
+
   return (
     <AppProvider>
       <NavigationContainer
+        ref={navigation}
+        onReady={() => {
+          routingInstrumentation.registerNavigationContainer(navigation);
+        }}
         children={
           <Stack.Navigator initialRouteName="Home">
             <Stack.Screen component={HomeScreen} name="Home" />
@@ -31,3 +51,5 @@ export function App() {
     </AppProvider>
   );
 }
+
+export const App = Sentry.wrap(ProvidedApp);
