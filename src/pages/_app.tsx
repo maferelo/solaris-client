62164a6b70214e968b9b2f70b164c41074507031
Felipe Restrepo <maferelo@outlook.com>
2024-03-20 19:16:13 -0500

    fix(sentry): install promise dependencies and remove error throwing in HomeScreen an sentry logs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,7 +23,7 @@ const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
 
 Sentry.init({
   dsn: "https://2360ae10da41b9c1950f60c859ce2122@o1208436.ingest.us.sentry.io/4506945207599104",
-  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+  debug: false, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
   integrations: [new Sentry.ReactNativeTracing({ routingInstrumentation })],
   // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
   tracesSampleRate: 1.0,
